5d671d6f82b19d5e2532609ba760160aaac71d47
Joziberto <jozibertoalvesteixeirafilho@gmail.com>
2025-05-27 20:07:41 -0300

    tailwind

--- introducao-nextjs/src/app/page.tsx
+++ introducao-nextjs/src/app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import Welcome from "@/componemtes/Welcome";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Welcome/>
+    <div>
+      {/* {NavBar} */}
+      <nav className="bg-amber-200 shadow-sm">
+        <div className="px-4">
+          <div className="flex justify-between h-16 items-center">
+            <div className="flex items-center">
+              <Image
+              src="/nextjs-icon.svg"
+              alt="Next.js logo"
+              width={40}
+              height={40}
+              className=""
+              />
+              <span className="ml-2">Next App</span>
+            </div>
+
+            <div>Links</div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
