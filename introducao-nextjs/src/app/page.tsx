6a4f1daceeceab710a18ad09e02bbaf4d46177ca
Joziberto <jozibertoalvesteixeirafilho@gmail.com>
2025-05-30 19:16:18 -0300

    Add NavBar component

--- introducao-nextjs/src/app/page.tsx
+++ introducao-nextjs/src/app/page.tsx
@@ -1,27 +1,9 @@
-import Image from "next/image";
+import NavBar from "@/componemtes/NavBar";
+
 
 export default function Home() {
   return (
-    <div>
-      {/* {NavBar} */}
-      <nav className="bg-amber-200 shadow-sm">
-        <div className="px-4">
-          <div className="flex justify-between h-16 items-center">
-            <div className="flex items-center">
-              <Image
-              src="/nextjs-icon.svg"
-              alt="Next.js logo"
-              width={40}
-              height={40}
-              className=""
-              />
-              <span className="ml-2">Next App</span>
-            </div>
-
-            <div>Links</div>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <NavBar/>
   );
 }
+
---
+++ introducao-nextjs/src/componemtes/NavBar.tsx
@@ -0,0 +1,38 @@
+import { link } from "fs"
+import Image from "next/image";
+
+export default function NavBar(){
+    return(
+    <div>
+        <nav className="bg-amber-200 shadow-sm">
+        <div className="px-4">
+            <div className="flex justify-between h-16 items-center">
+            
+            <div className="flex items-center">
+                <Image
+                src="/nextjs-icon.svg"
+                alt="Next.js logo"
+                width={40}
+                height={40}
+                className=""
+                />
+                <span className="ml-2">Next App</span>
+            </div>
+
+            <div className="flex space-x-6">
+                <a href="" className="text-lg font-medium hover:text-indigo-400">
+                Inicio
+                </a>
+                <a href="" className="text-lg font-medium hover:text-indigo-400">
+                Recursos
+                </a>
+                <a href="" className="text-lg font-medium hover:text-indigo-400">
+                contatos
+                </a>
+            </div>
+            </div>
+        </div>
+        </nav>
+    </div>
+    )
+}
