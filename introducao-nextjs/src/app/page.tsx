9cfea7e6d6c363fa9581445efece20d4d2414636
Joziberto <jozibertoalvesteixeirafilho@gmail.com>
2025-06-03 20:56:48 -0300

    atividade03

--- introducao-nextjs/src/app/page.tsx
+++ introducao-nextjs/src/app/page.tsx
@@ -1,11 +1,30 @@
 import NavBar from "@/componemtes/NavBar";
+import Cards from "@/componemtes/CreateCards";
 
 const link = [{href:'/', label:'Inicio'},{href:'/about', label:'Sobre nós'},{href:'/products', label:'Produtos'}]
+// const card = [{title:'Renderização Híbrida', content:'SSG, SSR, e ISR para otimizar performace e CEO.', imageURL:'🤡'}]
 
 export default function Home() {
   return(
   <div>
     <NavBar links = {link} />
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <Cards
+            title="Renderização Híbrida"
+            content="SSG, SSR e ISR para otimizar performance e SEO."
+            icon="🚀" />
+
+          <Cards
+            title="CSS Utility-First"
+            content="Tailwind CSS para estilização rápida e consistente."
+            icon="🎨" />
+
+          <Cards
+            title="TypeScript Integrado"
+            content="Tipagem estática para um código mais seguro."
+            icon="🔒" />
+    </div>
+    
   </div>
   );
 }
